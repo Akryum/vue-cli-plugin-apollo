cc0b063faa12bb052545558bfd64ee177d517043
NataliaTepluhina <tarya.se@gmail.com>
2018-10-20 12:23:09 -0400

    fix: Set apolloServerOptions.mockEntireSchema to false to preserve resolvers

--- generator/templates/api-server/default/apollo-server/mocks.js
+++ generator/templates/api-server/default/apollo-server/mocks.js
@@ -1,4 +1,4 @@
-// Enable mocking in vue.config.js with `"pluginOptions": { "graphqlMock": true }`
+// Enable mocking in vue.config.js with `"pluginOptions": { "enableMocks": true }`
 // Customize mocking: https://www.apollographql.com/docs/graphql-tools/mocking.html#Customizing-mocks
 export default {
   // Mock resolvers here
