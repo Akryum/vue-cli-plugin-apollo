cb86ae56acc1d0e37c04135c0816c00e24d363d8
Guillaume Chau <alphadelta.fg@gmail.com>
2018-10-29 16:57:58 +0100

    fix(generator): generate-schema arg

--- generator/index.js
+++ generator/index.js
@@ -23,8 +23,8 @@ module.exports = (api, options, rootOptions) => {
         'graphql-type-json': '^0.2.1',
       },
       scripts: {
-        'apollo:dev': 'vue-cli-service apollo:dev --generate',
-        'apollo:dev:client': 'vue-cli-service apollo:dev --run "vue-cli-service serve" --generate',
+        'apollo:dev': 'vue-cli-service apollo:dev --generate-schema',
+        'apollo:dev:client': 'vue-cli-service apollo:dev --run "vue-cli-service serve" --generate-schema',
         'apollo:start': 'vue-cli-service apollo:start',
         'apollo:schema:generate': 'vue-cli-service apollo:schema:generate',
         'apollo:schema:publish': 'vue-cli-service apollo:schema:publish',
