2397ea3d9863f5d63b6c21d0b358d7e750a7dd84
Peter Uithoven <peter@peteruithoven.nl>
2020-09-10 12:15:42 +0200

    Handle localStorage access failing

--- graphql-client/src/index.js
+++ graphql-client/src/index.js
@@ -213,9 +213,14 @@ export function restartWebsockets (wsClient) {
 
 function defaultGetAuth (tokenName) {
   if (typeof window !== 'undefined') {
-    // get the authentication token from local storage if it exists
-    const token = window.localStorage.getItem(tokenName)
-    // return the headers to the context so httpLink can read them
-    return token ? `Bearer ${token}` : ''
+    try {
+      // get the authentication token from local storage if it exists
+      const token = window.localStorage.getItem(tokenName)
+      // return the headers to the context so httpLink can read them
+      return token ? `Bearer ${token}` : ''
+    } catch (e) {
+      // ignore localStorage access failing.
+      return false
+    }
   }
 }
